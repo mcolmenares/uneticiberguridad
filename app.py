226bd129f0a796977d3afc2bf4c3670f533e5fa1
mcolmenares <maikincolmenares@gmail.com>
2022-10-20 14:05:30 -0400

    standarización de variables en las operaciones

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #prueba git 1
+
 from asyncio.log import logger
 from flask import Flask
 #app = flask.Flask(app)
@@ -29,25 +30,25 @@ def sum(val1, val2):
     
 #\n
 #////////////__RESTA__/////////////////////////////////////////////////#
-@app.route("/cal/res/<int:val3>/<int:val4>")
-def res(val3, val4):
-    reps = "La respuesta de {} - {} = {}\n".format(val3, val4, val3-val4)
+@app.route("/cal/res/<int:val1>/<int:val2>")
+def res(val1, val2):
+    reps = "La respuesta de {} - {} = {}\n".format(val1, val2, val1-val2)
     logger.info(reps)
     return reps 
     
 
 #////////////__MULTI__/////////////////////////////////////////////////#
-@app.route("/cal/mul/<int:val5>/<int:val6>")
-def mul(val5, val6):
-    reps = "La respuesta de {} * {} = {}\n".format(val5, val6, val5*val6)
+@app.route("/cal/mul/<int:val1>/<int:val2>")
+def mul(val1, val2):
+    reps = "La respuesta de {} * {} = {}\n".format(val1, val2, val1*val2)
     logger.info(reps)
     return reps
     
 
 #////////////__DIV__/////////////////////////////////////////////////#
-@app.route("/cal/div/<int:val7>/<int:val8>")
-def div(val7, val8):
-    reps = "La respuesta de {} / {} = {}\n".format(val7, val8, val7/val8)
+@app.route("/cal/div/<int:val1>/<int:val2>")
+def div(val1, val2):
+    reps = "La respuesta de {} / {} = {}\n".format(val1, val2, val1/val2)
     logger.info(reps)
     return reps
     
